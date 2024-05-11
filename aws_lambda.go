96bd661a746d875b79caa184ce01bed07470930f
Nitin Goyal <nitingoyal.dev@gmail.com>
2024-05-11 08:42:23 +0530

    code reformat

--- aws_lambda.go
+++ aws_lambda.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+
 	"github.com/ngoyal16/owlvault/config"
 	"github.com/ngoyal16/owlvault/routes"
-	"log"
 )
 
 var ginLambda *ginadapter.GinLambda
